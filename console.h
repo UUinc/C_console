33e7435479dd462a5d06dd7bb13dae0f3cd16dab
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-01-10 19:53:16 +0100

    Update console.h

--- console.h
+++ console.h
@@ -1,30 +1,27 @@
-#ifndef _INC_CONSOLE
-#define _INC_CONSOLE
+#ifndef CONSOLE_H
+#define CONSOLE_H
 
 #include<stdlib.h>
 #include<windows.h>
 
-/**
- * Colors which you can use in your application.
- */
 typedef enum
 {
-    BLACK,          /**< black color */
-    BLUE,           /**< blue color */
-    GREEN,          /**< green color */
-    CYAN,           /**< cyan color */
-    RED,            /**< red color */
-    MAGENTA,        /**< magenta color */
-    BROWN,          /**< brown color */
-    LIGHTGRAY,      /**< light gray color */
-    DARKGRAY,       /**< dark gray color */
-    LIGHTBLUE,      /**< light blue color */
-    LIGHTGREEN,     /**< light green color */
-    LIGHTCYAN,      /**< light cyan color */
-    LIGHTRED,       /**< light red color */
-    LIGHTMAGENTA,   /**< light magenta color */
-    YELLOW,         /**< yellow color */
-    WHITE           /**< white color */
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    BROWN,
+    LIGHTGRAY,
+    DARKGRAY,
+    LIGHTBLUE,
+    LIGHTGREEN,
+    LIGHTCYAN,
+    LIGHTRED,
+    LIGHTMAGENTA,
+    YELLOW,
+    WHITE
 } COLORS;
 
 static int __BACKGROUND = 0;
@@ -57,7 +54,6 @@ int wherey()
 
     return csbi.dwCursorPosition.Y;
 }
-//Console Color
 void consolecolor(char background[2], char text[2])
 {
 char command[10] = "Color ";
@@ -87,8 +83,8 @@ void clrscr()
 }
 void MaximizeOutputWindow(void)
 {
-    HWND consoleWindow = GetConsoleWindow(); // This gets the value Windows uses to identify your output window 
-    ShowWindow(consoleWindow, SW_MAXIMIZE); // this mimics clicking on its' maximize button
+    HWND consoleWindow = GetConsoleWindow();
+    ShowWindow(consoleWindow, SW_MAXIMIZE);
 }
 
 int GetFontSize(HANDLE windowHandle, COORD *size)

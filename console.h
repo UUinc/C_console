c72286a7e6d3a33cd9d72d974017ac2027b111f3
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-05-03 00:52:39 +0000

    ConsoleWidth, ConsoleHeight and ClearLine
    3 new functions added:
     - ConsoleWidth
     - ConsoleHeight
     - ClearLine

--- console.h
+++ console.h
@@ -120,4 +120,36 @@ void FontSize(short _size)
         SetFontSize(h, size);
     }
 }
+int ConsoleWidth()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    return columns;
+}
+int ConsoleHeight()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int rows;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+    return rows;
+}
+void ClearLine()
+{
+    int i;
+
+    gotoxy(console_width()-1, wherey());
+    
+    printf(" \b");
+    for (i=0; i<console_width(); i++)
+    {
+        printf("\b \b");
+    }
+}
 #endif
